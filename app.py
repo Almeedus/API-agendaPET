29fc23d2759ff0dccb377f96929a9b3e408b5366
Eduardo Almeida <almeedusa@gmail.com>
2025-05-30 15:45:35 -0300

    configuração de rotas e swagger para documentação de rotas

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flasgger import Swagger
+from routes.usuario.usuario_route import usuario_bp
 
 app = Flask(__name__)
+swagger = Swagger(app)
+
+app.register_blueprint(usuario_bp, url_prefix="/usuarios")
 
 @app.route("/")
-def hello():
-    return "Rota teste!"
+def home():
+    return "🚀 API AgendaPET está rodando!"
 
+if __name__ == "__main__":
+    app.run(debug=True)
