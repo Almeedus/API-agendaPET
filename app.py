32dc87c59ec37c74e3e5aea0086c6d34681f41f5
Eduardo Almeida <almeedusa@gmail.com>
2025-05-30 16:11:44 -0300

    adicionando rota vacinas

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flasgger import Swagger
 from routes.usuario.usuario_route import usuario_bp
+from routes.vacinas.vacinas_route import vacinas_bp
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 app.register_blueprint(usuario_bp, url_prefix="/usuarios")
+app.register_blueprint(vacinas_bp, url_prefix="/vacinas") 
 
 @app.route("/")
 def home():
