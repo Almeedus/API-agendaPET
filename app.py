3cdc37b6642cba77d3e2db78e7c03420cf2ae5a3
Eduardo Almeida <almeedusa@gmail.com>
2025-05-30 16:51:20 -0300

    Adicionando rota de Pet

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flasgger import Swagger
 from routes.usuario.usuario_route import usuario_bp
 from routes.vacinas.vacinas_route import vacinas_bp
 from routes.pet_usuario.pet_usuario_route import pet_usuario_bp
+from routes.pet.pet_route import pet_bp
+
 
 
 
@@ -12,7 +14,7 @@ swagger = Swagger(app)
 app.register_blueprint(usuario_bp, url_prefix="/usuarios")
 app.register_blueprint(vacinas_bp, url_prefix="/vacinas") 
 app.register_blueprint(pet_usuario_bp, url_prefix="/pet_usuario")
-
+app.register_blueprint(pet_bp, url_prefix="/pets")
 
 @app.route("/")
 def home():
